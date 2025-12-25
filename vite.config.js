4170cdb9e5fba434f8384507c9adb4e0988271c5
yashsolanki12 <yash@ecodesoft.com>
2025-12-25 11:39:37 +0530

    Comment the unused import in vite config file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { visualizer } from "rollup-plugin-visualizer";
+// import { visualizer } from "rollup-plugin-visualizer";
 
 // Related: https://github.com/remix-run/remix/issues/2835#issuecomment-1144102176
 // Replace the HOST env var with SHOPIFY_APP_URL so that it doesn't break the Vite server.
