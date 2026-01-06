8103f6f9b798fe91fec7b668add2075a38013daa
yashsolanki12 <yash@ecodesoft.com>
2026-01-06 18:15:12 +0530

    Integrate phone list page changes with app embed

--- app/shopify.server.js
+++ app/shopify.server.js
@@ -11,7 +11,7 @@ const shopify = shopifyApp({
   apiKey: process.env.SHOPIFY_API_KEY,
   apiSecretKey: process.env.SHOPIFY_API_SECRET || "",
   apiVersion: ApiVersion.October25,
-  scopes: process.env.SCOPES?.split(","),
+  scopes: (process.env.SCOPES?.split(",") || []).concat("read_themes"),
   appUrl: process.env.SHOPIFY_APP_URL || "",
   authPathPrefix: "/auth",
   sessionStorage: new MongoDBSessionStorage(
